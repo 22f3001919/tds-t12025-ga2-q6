009105e1fb12d3119b24068d0881074e5fabaa01
Carlton DSilva <123925855+22f3001919@users.noreply.github.com>
2025-01-20 10:30:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,37 @@
 # api/index.py
 import json
 from http.server import BaseHTTPRequestHandler
+import urllib.parse
+
+file_path = 'q-vercel-python.json'
+with open (file_path, 'r') as file:
+    data = json.load (file)
+
+marks = dict ()
+for i in range (len (data)):
+    marks[data[i]['name']] =  data[i]['marks']
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+    # def do_GET(self):
+    #     self.send_response(200)
+    #     self.send_header('Content-type','application/json')
+    #     self.end_headers()
+    #     self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
+    #     return
+    def do_GET (self):
+        parsed_url = urllib.parse.urlparse(self.path)
+        query_params = urllib.parse.parse_qs(parsed_url.query)
+        names = query_params.get ('name', [])
+        
+        query_marks = list ()
+        for n in names:
+            query_marks.append (marks[n])
+
+        self.send_response (200)
+        self.send_header ('Content-type', 'application/json')
+        self.end_headers ()
+        response = {
+            "marks": query_marks,
+        }
+        self.wfile.write (json.dumps (response).encode ('utf-8'))
         return
